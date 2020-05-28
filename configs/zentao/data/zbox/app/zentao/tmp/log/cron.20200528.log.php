230710261f207d5ffc62b5333a3697cb1e269d06
mark <476178697@qq.com>
2020-05-28 14:19:41 +0800

    去除env

--- configs/zentao/data/zbox/app/zentao/tmp/log/cron.20200528.log.php
+++ configs/zentao/data/zbox/app/zentao/tmp/log/cron.20200528.log.php
@@ -567,3 +567,543 @@ command: moduleName=ci&methodName=exec.
 return : .
 output : 
 success
+12:40:02 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+12:40:02 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+12:40:02 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+12:40:02 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+12:40:02 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+12:46:00 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+12:46:00 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+12:46:00 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+12:46:00 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+12:46:00 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+12:51:00 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+12:51:00 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+12:51:00 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+12:51:00 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+12:51:00 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+12:56:00 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+12:56:00 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+12:56:00 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+12:56:00 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+12:56:00 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:01:00 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:01:00 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:01:00 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:01:00 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:01:00 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:06:00 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:06:00 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:06:00 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:06:00 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:06:00 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:11:00 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:11:00 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:11:00 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:11:00 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:11:00 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:16:00 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:16:00 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:16:00 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:16:00 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:16:00 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:21:00 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:21:00 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:21:00 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:21:00 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:21:00 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:25:59 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:25:59 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:25:59 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:25:59 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:25:59 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:30:58 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:30:58 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:30:58 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:30:58 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:30:58 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:35:55 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:35:55 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:35:55 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:35:55 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:35:55 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:40:53 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:40:53 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:40:53 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:40:53 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:40:53 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:45:53 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:45:53 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:45:53 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:45:53 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:45:53 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:50:53 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:50:53 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:50:53 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:50:53 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:50:53 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+13:55:53 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+13:55:53 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+13:55:53 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+13:55:53 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+13:55:53 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+14:00:53 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+14:00:53 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+14:00:53 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+14:00:53 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+14:00:53 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+14:05:53 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+14:05:53 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+14:05:53 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+14:05:53 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+14:05:53 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+14:10:53 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+14:10:53 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+14:10:53 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+14:10:53 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+14:10:53 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
+14:15:53 task 7 executed,
+command: moduleName=mail&methodName=asyncSend.
+return : .
+output : 
+OK
+
+14:15:53 task 8 executed,
+command: moduleName=webhook&methodName=asyncSend.
+return : .
+output : 
+NO WEBHOOK EXIST.
+
+14:15:53 task 9 executed,
+command: moduleName=admin&methodName=deleteLog.
+return : .
+output : 
+
+14:15:53 task 12 executed,
+command: moduleName=ci&methodName=checkCompileStatus.
+return : .
+output : 
+success
+14:15:53 task 13 executed,
+command: moduleName=ci&methodName=exec.
+return : .
+output : 
+success
